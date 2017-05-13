06f7351c3c2f95aff82505e2f1860116ddf8a99e
neonew <neonew@gmail.com>
2017-05-13 05:47:13 +0200

    added compass image; listen to sensors

--- app/src/main/java/de/neonew/compass/MainActivity.kt
+++ app/src/main/java/de/neonew/compass/MainActivity.kt
@@ -1,12 +1,85 @@
 package de.neonew.compass
 
+import android.hardware.Sensor
+import android.hardware.Sensor.TYPE_ACCELEROMETER
+import android.hardware.Sensor.TYPE_MAGNETIC_FIELD
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.hardware.SensorManager.SENSOR_DELAY_GAME
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    lateinit var sensorManager: SensorManager
+    lateinit var image: ImageView
+    lateinit var accelerometer: Sensor
+    lateinit var magnetometer: Sensor
+
+    var currentDegree = 0.0f
+    var lastAccelerometer = FloatArray(3)
+    var lastMagnetometer = FloatArray(3)
+    var lastAccelerometerSet = false
+    var lastMagnetometerSet = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        image = findViewById(R.id.imageViewCompass) as ImageView
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(TYPE_ACCELEROMETER)
+        magnetometer = sensorManager.getDefaultSensor(TYPE_MAGNETIC_FIELD)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        sensorManager.registerListener(this, accelerometer, SENSOR_DELAY_GAME)
+        sensorManager.registerListener(this, magnetometer, SENSOR_DELAY_GAME)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this, accelerometer)
+        sensorManager.unregisterListener(this, magnetometer)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event!!.sensor === accelerometer) {
+            System.arraycopy(event!!.values, 0, lastAccelerometer, 0, event.values.size)
+            lastAccelerometerSet = true
+        } else if (event!!.sensor === magnetometer) {
+            System.arraycopy(event!!.values, 0, lastMagnetometer, 0, event.values.size)
+            lastMagnetometerSet = true
+        }
+
+        if (lastAccelerometerSet && lastMagnetometerSet) {
+            val r = FloatArray(9)
+            SensorManager.getRotationMatrix(r, null, lastAccelerometer, lastMagnetometer)
+
+            val orientation = FloatArray(3)
+            SensorManager.getOrientation(r, orientation)
+            val azimuthInRadians = orientation[0]
+            val azimuthInDegress = (Math.toDegrees(azimuthInRadians.toDouble()) + 360).toFloat() % 360
+
+            val rotateAnimation = RotateAnimation(
+                    currentDegree,
+                    -azimuthInDegress,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f)
+            rotateAnimation.duration = 1000
+            rotateAnimation.fillAfter = true
+
+            image.startAnimation(rotateAnimation)
+            currentDegree = -azimuthInDegress
+        }
     }
 }
